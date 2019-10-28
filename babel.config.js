98f950b53bf8318b4eb0bf99df0affe598dfb56c
Aleksei Arro <polsck@gmail.com>
2019-10-28 20:50:34 +0300

    Babel base configuration added

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
+// TODO: add corejs3
+
 module.exports = {
+  ignore: ['**/node_modules/**'],
   presets: [
-    '@babel/preset-env',
+    [
+      '@babel/preset-env', {
+        loose: false,
+        modules: false,
+      },
+    ],
   ],
 };
