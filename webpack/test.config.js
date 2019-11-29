d792ebce65c670fd3d8e44d3e1b56615c8ef86a9
Aleksei Arro <polsck@gmail.com>
2019-11-29 22:12:04 +0300

    npm packages updated, Webpack test.config.js added

---
+++ webpack/test.config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const path = require('path');
+
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const { path: basePath } = require('./base.config');
+
+module.exports = {
+  mode: 'development',
+
+  target: 'node',
+
+  entry: path.join(basePath.SRC, 'tests.js'),
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+      },
+    ],
+  },
+
+  output: {
+    filename: 'test_output.js',
+    path: basePath.TEST,
+  },
+
+  plugins: [
+    new CaseSensitivePathsPlugin(),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      '@': basePath.SRC,
+    },
+  },
+
+  devtool: false,
+
+  stats: {
+    assets: false,
+    entrypoints: false,
+    modules: false,
+  },
+};
