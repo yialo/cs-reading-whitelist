1bfda9b51687dcb68f8862d473aad2acfaefe274
Aleksei Arro <polsck@gmail.com>
2020-01-01 15:09:47 +0300

    ESLint resolver reason detected: webpack config do not applies correctly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import '@csstools/normalize.css';
 import '#css/index.css';
 
-import showMessage from './js/components/testing.js';
+import showMessage from './js/components/testi.js';
 
 showMessage();
