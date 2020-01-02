c98daf90bd9318b03b2cb9fad7d94781c293d8f9
Aleksei Arro <polsck@gmail.com>
2020-01-02 20:42:17 +0300

    Unnecessary static Pug markup removed

--- src/js/components/Subject.jsx
+++ src/js/components/Subject.jsx
@@ -6,6 +6,11 @@ export default class Subject extends React.Component {
       <li className="chapter__item">
         <a href={this.props.url}>{this.props.caption}</a>
         {this.props.legend ? ` - ${this.props.legend}` : ''}
+        <ul className="s_list chapter__tags">
+          {this.props.tags.map((tag, i) => (
+            <li className="chapter__tag" key={i}> #{tag}</li>
+          ))}
+        </ul>
       </li>
     );
   }
