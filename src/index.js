faaef05988382a86f4d946294d1dbe2f4430bbdc
Aleksei Arro <polsck@gmail.com>
2020-01-02 17:38:42 +0300

    index.js: unnecessary statements removed

--- src/index.js
+++ src/index.js
@@ -4,11 +4,5 @@ import '#css/index.css';
 import showMessage from '#components/testing.js';
 import greet from './js/simple.js';
 
-const init = async () => {
-  showMessage();
-  greet();
-};
-
-init().catch((err) => {
-  console.log(err.message);
-});
+showMessage();
+greet();
