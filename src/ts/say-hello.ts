ae99fe1287c78942cbc2f251b8480af57ba47205
Aleksei Arro <polsck@gmail.com>
2020-01-28 23:23:19 +0300

    ESLint adjusted to check TS(X) files with CLI

--- src/ts/say-hello.ts
+++ src/ts/say-hello.ts
@@ -1,15 +1,15 @@
-const greeting: string = 'Hello from the Types!';
+const greeting = 'Hello from the Types!';
 
 interface Message {
-  field: string,
-  digit: number,
+  field: string;
+  digit: number;
 }
 
 const message: Message = {
   field: greeting,
-  digit: 23
+  digit: 23,
 };
 
-export default () => {
+export default (): void => {
   console.log('[TypeScript]:', message);
 };
