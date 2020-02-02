b5fb29e3e041d041f7d400e661bdd302a5326636
Aleksei Arro <polsck@gmail.com>
2020-02-02 22:15:36 +0300

    React class components reworked to function ones

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -5,8 +5,6 @@ import FilterForm from './FilterForm.jsx';
 import Subjects from './Subjects.jsx';
 import SubjectsItem from './SubjectsItem.jsx';
 
-const DEFAULT_LIST_LENGTH_INCREMENT = 10;
-
 const FILTER_LIST = [
   ['none', '[отключена]'],
   ['caption', 'по названию'],
@@ -16,71 +14,38 @@ const FILTER_LIST = [
   ['legend', 'по описанию'],
 ];
 
-export default class App extends React.PureComponent {
-  static propTypes = {
-    list: PropTypes.arrayOf(
-        PropTypes.objectOf(
-            PropTypes.oneOfType([
-              PropTypes.arrayOf(PropTypes.string),
-              PropTypes.string,
-            ])
-        )
-    )
-      .isRequired,
-  };
-
-  state = {
-    // filter: FILTER_LIST[0][0],
-    listLength: (
-      this._fullList.length < DEFAULT_LIST_LENGTH_INCREMENT
-        ? this._fullList.length
-        : DEFAULT_LIST_LENGTH_INCREMENT
-    ),
-  };
-
-  constructor(props) {
-    super(props);
-
-    this._fullList = props.list;
-  }
-
-  getCurrentList() {
-    return this._fullList.slice(0, this.state.listLength);
-  }
-
-  onLoadMore = () => {
-    this.setState((state) => {
-      const listRestLength = this._fullList.length - state.listLength;
-
-      const increment = listRestLength < DEFAULT_LIST_LENGTH_INCREMENT
-        ? listRestLength
-        : DEFAULT_LIST_LENGTH_INCREMENT;
-
-      return {
-        listLength: state.listLength + increment,
-      };
-    });
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <h1 className="page__heading">Computer Science Reading Whitelist</h1>
-        <main className="page-content page__content">
-          <FilterForm list={FILTER_LIST} />
-          <Subjects onButtonClick={this.onLoadMore}>
-            {this.getCurrentList().map((it, i) => (
-              <SubjectsItem
-                key={`${it.lang}-${i + 1}`}
-                caption={it.caption}
-                legend={it.legend}
-                tags={it.tags}
-                url={it.url}
-              />
-            ))}
-          </Subjects>
-        </main>
-      </React.Fragment>
-    );
-  }
+function App(props) {
+  return (
+    <React.Fragment>
+      <h1 className="page__heading">Computer Science Reading Whitelist</h1>
+      <main className="page-content page__content">
+        <FilterForm list={FILTER_LIST} />
+        <Subjects>
+          {props.list.map((it, i) => (
+            <SubjectsItem
+              key={`${it.lang}-${i + 1}`}
+              caption={it.caption}
+              legend={it.legend}
+              tags={it.tags}
+              url={it.url}
+            />
+          ))}
+        </Subjects>
+      </main>
+    </React.Fragment>
+  );
 }
+
+App.propTypes = {
+  list: PropTypes.arrayOf(
+      PropTypes.objectOf(
+          PropTypes.oneOfType([
+            PropTypes.arrayOf(PropTypes.string),
+            PropTypes.string,
+          ])
+      )
+  )
+    .isRequired,
+};
+
+export default App;
--- src/js/components/Subjects.jsx
+++ src/js/components/Subjects.jsx
@@ -1,20 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import Button from './Button.jsx';
-
 function Subjects(props) {
   return (
     <div className="subjects page-content__subjects">
       <ul className="s_list subjects__list">
         {props.children}
       </ul>
-      <Button
-        classNames={['subjects__button']}
-        type="submit"
-        legend="Загрузить ещё"
-        onClick={props.onButtonClick}
-      />
     </div>
   );
 }
@@ -25,7 +17,6 @@ Subjects.defaultProps = {
 
 Subjects.propTypes = {
   children: PropTypes.node,
-  onButtonClick: PropTypes.func.isRequired,
 };
 
 export default Subjects;
