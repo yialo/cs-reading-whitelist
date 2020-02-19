6f783d6eaedac82945bb7a15075e0740cbf00d0f
Aleksei Arro <polsck@gmail.com>
2020-02-19 23:00:50 +0300

    'sum' function test suite added

--- src/js/logics/intro.test.js
+++ src/js/logics/intro.test.js
@@ -6,3 +6,16 @@ const { sum } = require('./intro.js');
 test('Should return sum of two values', () => {
   expect(sum(1, 3)).toBe(4);
 });
+
+
+test('Should return value correctly compared with another', () => {
+  expect(sum(2, 3)).toBeGreaterThan(4);
+  expect(sum(2, 3)).toBeGreaterThanOrEqual(5);
+  expect(sum(2, 3)).toBeLessThan(7);
+  expect(sum(2, 3)).toBeLessThanOrEqual(5);
+});
+
+test('Should sum float values correctly', () => {
+  expect(sum(0.5, 0.25)).toBe(0.75);
+  expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
+});
