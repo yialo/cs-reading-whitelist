d1ba53eb1a17d0eb7c04252dc0b15e86f166d10d
Aleksei Arro <polsck@gmail.com>
2020-02-19 23:09:29 +0300

    Simple matchers learned

--- src/js/logics/intro.test.js
+++ src/js/logics/intro.test.js
@@ -1,21 +1,34 @@
 /* eslint-disable */
 'use strict';
 
-const { sum } = require('./intro.js');
+const { sum, nativeNull } = require('./intro.js');
 
-test('Should return sum of two values', () => {
-  expect(sum(1, 3)).toBe(4);
+describe('Sums two numeric values', () => {
+  test('should return sum of two values', () => {
+    expect(sum(1, 3)).toBe(4);
+  });
+
+
+  test('should return value correctly compared with another', () => {
+    expect(sum(2, 3)).toBeGreaterThan(4);
+    expect(sum(2, 3)).toBeGreaterThanOrEqual(5);
+    expect(sum(2, 3)).toBeLessThan(7);
+    expect(sum(2, 3)).toBeLessThanOrEqual(5);
+  });
+
+  test('should sum float values correctly', () => {
+    expect(sum(0.5, 0.25)).toBe(0.75);
+    expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
+  });
 });
 
-
-test('Should return value correctly compared with another', () => {
-  expect(sum(2, 3)).toBeGreaterThan(4);
-  expect(sum(2, 3)).toBeGreaterThanOrEqual(5);
-  expect(sum(2, 3)).toBeLessThan(7);
-  expect(sum(2, 3)).toBeLessThanOrEqual(5);
-});
-
-test('Should sum float values correctly', () => {
-  expect(sum(0.5, 0.25)).toBe(0.75);
-  expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
+describe('Returns null', () => {
+  test('should return null value', () => {
+    expect(nativeNull()).toBe(null);
+    expect(nativeNull()).toBeNull();
+    expect(nativeNull()).toBeFalsy();
+    expect(nativeNull()).toBeDefined();
+    expect(nativeNull()).not.toBeTruthy();
+    expect(nativeNull()).not.toBeUndefined();
+  });
 });
