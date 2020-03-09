72d73ce887e2304450b023338beafc25cb017f04
Aleksei Arro <polsck@gmail.com>
2020-03-09 14:45:19 +0300

    Added type check for ctx param

--- extra/playground/bind.js
+++ extra/playground/bind.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 /* eslint-disable strict */
 'use strict';
 
@@ -5,18 +6,26 @@ const person = {
   name: 'Bob',
 };
 
+function simple(tel, email) {
+  console.log(`Name: Basil, tel: ${tel}, email: ${email}`);
+}
+
 function info(tel, email) {
   console.log(`Name: ${this.name}, tel: ${tel}, email: ${email}`);
 }
 
 function bind(fn, ctx, ...boundArgs) {
   return function (...args) {
+    if (ctx === null || typeof ctx !== 'object') {
+      return fn(...boundArgs, ...args);
+    }
     const field = Symbol('field');
     ctx[field] = fn;
     return ctx[field](...boundArgs, ...args);
   };
 }
 
+bind(simple, null)('123456', 'bob@gmail.com');
 bind(info, person)('123456', 'bob@gmail.com');
 bind(info, person, '123456')('bob@gmail.com');
 bind(info, person, '123456', 'bob@gmail.com')();
