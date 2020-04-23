672f7476464ce44d16ab629799ad2262634671f8
Aleksei Arro <polsck@gmail.com>
2020-04-23 18:15:31 +0300

    ErrorBoundary component added

--- src/js/init-app.jsx
+++ src/js/init-app.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App/WithStore.jsx';
+import ErrorBoundary from './components/ErrorBoundary/index.jsx';
 
 export default () => {
   const $root = document.getElementById('app-root');
   const app = (
     <React.StrictMode>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </React.StrictMode>
   );
   ReactDOM.render(app, $root);
