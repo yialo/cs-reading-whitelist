6f95b34bcd3e35259839ad2dd2b6cc7a999389a9
Aleksei Arro <polsck@gmail.com>
2020-06-13 00:51:48 +0300

    Remove obsolete font import

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import '@csstools/normalize.css';
-
 import 'css/index.css';
-
-import './fonts/source-sans-pro_400_italic.woff2';
-import './fonts/source-sans-pro_400_normal.woff2';
-import './fonts/source-sans-pro_700_italic.woff2';
-import './fonts/source-sans-pro_700_normal.woff2';
 
 import initApp from './ts/initApp.tsx';
 
