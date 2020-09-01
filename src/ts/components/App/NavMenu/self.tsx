19fa40adb4cdfd8e28e98fea6e48b08e9a12fd43
Aleksei Arro <polsck@gmail.com>
2020-09-01 21:13:56 +0300

    Add mixin for theme

--- src/ts/components/App/NavMenu/self.tsx
+++ src/ts/components/App/NavMenu/self.tsx
@@ -34,7 +34,18 @@ export const AppNavMenu: React.FC<IProps> = ({ className }) => {
         className={style.button}
         type="button"
         onClick={handleMenuExpansion}
-      />
+      >
+        <svg
+          aria-hidden="true"
+          className={style.icon}
+          viewBox="0 0 24 24"
+          width="100%"
+          height="100%"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+        </svg>
+      </button>
       <ul className={style.list} id={buttonId}>
         {routeList.map(([route, label]) => (
           <li key={label}>
