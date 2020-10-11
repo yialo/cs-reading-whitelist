d6a7edad17399af325eca51c318a42c7966dd3b3
Aleksei Arro <polsck@gmail.com>
2020-10-11 21:15:42 +0300

    Extract style for page headline

--- src/ts/components/pages/naming/NamingPage/self.tsx
+++ src/ts/components/pages/naming/NamingPage/self.tsx
@@ -9,7 +9,7 @@ import style from './style.scss';
 export const NamingPage: React.FC = () => {
   return (
     <main className={style.root}>
-      <h1>Типовые названия UI-компонентов</h1>
+      <h1 className={style.headline}>Типовые названия UI-компонентов</h1>
       <hr />
       <ReferenceLinks />
       <hr />
