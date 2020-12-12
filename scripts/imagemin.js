05dc80a5eb9a0ebb790e19de10b1ef6e2ef47de3
Aleksei Arro <polsck@gmail.com>
2020-12-12 16:31:32 +0300

    Remove gifsicle plugin from imagemin

--- scripts/imagemin.js
+++ scripts/imagemin.js
@@ -2,7 +2,6 @@
 
 const { src, dest } = require('gulp');
 const imagemin = require('gulp-imagemin');
-const gifsicle = require('imagemin-gifsicle');
 const jpegtran = require('imagemin-jpegtran');
 const mozjpeg = require('imagemin-mozjpeg');
 const pngquant = require('imagemin-pngquant');
@@ -14,7 +13,6 @@ const svgoOptions = require('./svgoOptions.js');
 const minify = () => (
   src('../minify/in/*.{jp?(e)g,png,svg}')
     .pipe(imagemin([
-      gifsicle(),
       jpegtran({ progressive: true }),
       mozjpeg({ quality: 90 }),
       pngquant({ speed: 1, quality: [0.8, 0.8] }),
