412ae07cd942b0be9248614955d288d67a5e3564
Aleksei Arro <polsck@gmail.com>
2021-09-12 00:05:27 +0300

    tools: clarify ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 'use strict';
 
+const tsParserOptions = {
+  tsconfigRootDir: __dirname,
+  project: ['./tsconfig.json'],
+};
+
+const srcResolverSettings = {
+  'import/resolver': {
+    webpack: {
+      config: './webpack.config.js',
+    },
+  },
+};
+
 module.exports = {
-  extends: ['yialo/preset-web-babel'],
   ignorePatterns: [
     '*.html',
     '/dist/',
     '/extra/',
   ],
-  parserOptions: {
-    babelOptions: {
-      configFile: './babel.config.js',
-    },
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-  },
   overrides: [
-    {
-      files: ['./**/*.{spec,test}.ts?(x)'],
-      extends: ['yialo/preset-web-ts-check-jest'],
-    },
     {
       files: [
         './*.js',
-        './config/**/*.js',
-        './scripts/**/*.js',
       ],
       extends: ['yialo/preset-node'],
       settings: {
@@ -31,15 +30,26 @@ module.exports = {
       },
     },
     {
+      files: ['./src/**/*.js?(x)'],
+      extends: ['yialo/preset-web-babel'],
+      parserOptions: {
+        babelOptions: {
+          configFile: './babel.config.js',
+        },
+      },
+      settings: srcResolverSettings,
+    },
+    {
       files: ['./src/**/*.ts?(x)'],
       extends: ['yialo/preset-web-ts-check'],
+      parserOptions: tsParserOptions,
+      settings: srcResolverSettings,
+    },
+    {
+      files: ['./**/*.{spec,test}.ts?(x)'],
+      extends: ['yialo/preset-web-ts-check-jest'],
+      parserOptions: tsParserOptions,
+      settings: srcResolverSettings,
     },
   ],
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: './webpack.config.js',
-      },
-    },
-  },
 };
