cb919c91995fa88066c1fd4c35d888edb1658b40
Aleksei Arro <polsck@gmail.com>
2021-11-01 14:03:32 +0300

    fix: Naming content navigation

--- src/features/naming/components/c_contents.tsx
+++ src/features/naming/components/c_contents.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 
-import commonStyle from '@/scss/scaffold.scss';
+import { Button } from '@/components/c_button';
 
 import { selectContentHeaders } from '../ducks/selectors';
 
+import commonStyle from '@/scss/scaffold.scss';
 import utilStyle from '../utils.scss';
 
 export const NamingContents: React.FC = () => {
@@ -15,9 +16,24 @@ export const NamingContents: React.FC = () => {
       <h2>Содержание</h2>
 
       <ul className={utilStyle.list}>
-        {headers.map((header, i) => (
-          <li key={i}>
-            <a className={commonStyle.link} href={`#${header.id}`}>{header.title}</a>
+        {headers.map(({ id, title }) => (
+          <li key={id}>
+            <Button
+              className={commonStyle.link}
+              role="link"
+              onClick={() => {
+                const $targetElement = document.getElementById(id);
+
+                if ($targetElement) {
+                  window.scrollTo({
+                    top: $targetElement.offsetTop,
+                    behavior: 'smooth',
+                  });
+                }
+              }}
+            >
+              {title}
+            </Button>
           </li>
         ))}
       </ul>
