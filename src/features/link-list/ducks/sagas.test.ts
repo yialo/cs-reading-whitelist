44fb1f6004f6643f0291e26638d164627d09c452
Aleksei Arro <polsck@gmail.com>
2021-11-05 20:22:30 +0300

    test: add fetchLinkListWorker step-by-step suite

---
+++ src/features/link-list/ducks/sagas.test.ts
@@ -0,0 +1,30 @@
+import { call, put } from 'redux-saga/effects';
+
+import { ACTION_TYPE } from './action-types';
+import { fetchLinkListWorker, getSubjectsFromApi } from './sagas';
+
+describe('fetchLinkListWorker step-by-step test', () => {
+  const gen = fetchLinkListWorker();
+
+  it('should call getSubjectsFromApi on 1st step', () => {
+    const result = gen.next().value;
+    const expected = call(getSubjectsFromApi);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should dispatch success action on 2nd step', () => {
+    const responseDataMock = {
+      data: [],
+    };
+
+    const result = gen.next(responseDataMock).value;
+
+    const expected = put({
+      type: ACTION_TYPE.FETCH_SUCCESS,
+      payload: [],
+    });
+
+    expect(result).toEqual(expected);
+  });
+});
