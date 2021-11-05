e5c5b2a0d13fe1208a16413f4e87747e54f82c1c
Aleksei Arro <polsck@gmail.com>
2021-11-05 13:27:56 +0300

    feat: update React Router to v6

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,10 @@
+import { Values } from '@/types/utils';
+
 export const ROUTES = {
   HOME: __GLOBAL_ENV_VARIABLE__PUBLIC_PATH__,
   NAMING: `${__GLOBAL_ENV_VARIABLE__PUBLIC_PATH__}naming`,
-};
+} as const;
+
+export type TRoute = Values<typeof ROUTES>;
 
 export const LIST_PAGE_SIZE = 10;
