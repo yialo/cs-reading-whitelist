e83eb794a82acf157e9c848b1ffb8d4ed7783ffc
Aleksei Arro <polsck@gmail.com>
2021-11-15 14:44:27 +0300

    refactor: revert MainButton component name

--- src/components/c_main-button/self.tsx
+++ src/components/c_main-button/self.tsx
@@ -5,7 +5,7 @@ import { Button, TButtonProps } from '@/components/c_button';
 
 import style from './style.scss';
 
-export const ButtonBase: React.FC<TButtonProps> = ({ className, ...propsRest }) => {
+export const MainButton: React.FC<TButtonProps> = ({ className, ...propsRest }) => {
   return (
     <Button className={cn(style.root, className)} {...propsRest} />
   );
