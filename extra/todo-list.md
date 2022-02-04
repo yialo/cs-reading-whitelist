4c483ef0f15f35d02573841852c30bb389d1aabf
Aleksei Arro <polsck@gmail.com>
2022-02-04 16:39:23 +0300

    feat: make page header translucent on scroll

--- extra/todo-list.md
+++ extra/todo-list.md
@@ -1,6 +1,5 @@
 # Todo list
 
-* Add transparency to Header
 * Add [Prettier](https://create-react-app.dev/docs/setting-up-your-editor/#formatting-code-automatically)
 * Format LinkList item as card
   * Add optional **author** field and sorting by author
--- src/main/components/c_header/self.tsx
+++ src/main/components/c_header/self.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import cn from 'clsx';
+import debounce from 'lodash/debounce';
 
 import type { TClassNameable } from '@/types/common';
 
@@ -8,9 +9,39 @@ import { ThemeToggle } from '../c_theme-toggle';
 
 import style from './self.scss';
 
+const SCROLL_THRESHOLD = 100;
+const SCROLL_DEBOUNCE_DELAY = 25;
+
 export const RootHeader: React.FC<TClassNameable> = ({ className }) => {
+  const [isTranslucent, setIsTranslucent] = React.useState(false);
+
+  React.useEffect(() => {
+    const handleScroll = debounce((event: Event) => {
+      if (!(event.target instanceof HTMLBodyElement)) {
+        return;
+      }
+
+      const shouldBeTranslucent = event.target.scrollTop > SCROLL_THRESHOLD;
+      setIsTranslucent(shouldBeTranslucent);
+    }, SCROLL_DEBOUNCE_DELAY);
+
+    document.body.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.body.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={cn(style.root, className)}>
+    <header
+      className={cn(
+        style.root,
+        {
+          [style.root_translucent!]: isTranslucent,
+        },
+        className,
+      )}
+    >
       <AppNavMenu />
       <ThemeToggle className={style.themeToggle} />
     </header>
