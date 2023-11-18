83f12562a23ea246d3f0cec37a12b5b409a1e638
Aleksei Arro <polsck@gmail.com>
2023-11-18 15:51:41 +0400

    chore: bump babel-related dependencies

--- babel.config.cjs
+++ babel.config.cjs
@@ -15,7 +15,7 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        corejs: '3.30',
+        corejs: '3.33',
         modules: process.env.BABEL_ENV === 'test' ? 'auto' : false,
         useBuiltIns: 'usage',
       },
