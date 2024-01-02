d7e3cc2aeaf45576e2c020e44f18d37270f72d83
Aleksei Arro <polsck@gmail.com>
2024-01-02 16:49:23 +0400

    refactor: recompose shared utils

--- src/pages/link-list/config.ts
+++ src/pages/link-list/config.ts
@@ -1,6 +1,6 @@
+import type { Values } from '@type-utils';
 import type { Subject } from '@/entities/subject';
-import type { TProcess } from '@/shared/config';
-import type { Values } from '@/types/utils';
+import type { Process } from '@/shared/config';
 
 export const LIST_PAGE_SIZE = 10;
 
@@ -22,7 +22,7 @@ export type TSorting = Values<typeof SORTING>;
 export interface LinkListState {
   list: Subject[];
   error: Error | null;
-  process: TProcess;
+  process: Process;
   searchString: string;
   filter: TFilter;
   sorting: TSorting;
