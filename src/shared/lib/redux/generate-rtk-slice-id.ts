0ed59f314a5ef7db4f788ba59ce8780facdc8e23
Aleksei Arro <polsck@gmail.com>
2024-02-11 15:59:43 +0400

    feat: scaffold redux utilities

---
+++ src/shared/lib/redux/generate-rtk-slice-id.ts
@@ -0,0 +1,10 @@
+export const generateRtkSliceId = (() => {
+  let count = 1;
+
+  return (sliceName = '') => {
+    const suffix = sliceName ? ` ${sliceName}` : '';
+    const id = `[s-${count}]${suffix}`;
+    count += 1;
+    return id;
+  };
+})();
---
+++ src/shared/lib/redux/index.ts
@@ -0,0 +1,2 @@
+export { RtkReducerManager } from './rtk-reducer-manager';
+export { generateRtkSliceId } from './generate-rtk-slice-id';
