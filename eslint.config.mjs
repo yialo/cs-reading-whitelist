c34e60947fdd37839537f28e6e1f781f35676adb
Aleksei Arro <polsck@gmail.com>
2025-06-08 16:04:56 +0400

    chore: draft ESLint flat config files

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+
+/** @type {import('@eslint/config-helpers').Config} */
+export default defineConfig({
+  files: [],
+  plugins: {
+    js,
+  },
+  extends: [],
+  rules: {},
+});
