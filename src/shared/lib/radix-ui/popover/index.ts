027fac2f6ab322e1cf98efa74e119ab2f44935e4
Aleksei Arro <polsck@gmail.com>
2025-08-26 21:56:45 +0400

    chore: add RadixUI Popover

---
+++ src/shared/lib/radix-ui/popover/index.ts
@@ -0,0 +1,34 @@
+import {
+  Popover,
+  PopoverAnchor,
+  PopoverTrigger,
+  PopoverPortal,
+  PopoverContent,
+  PopoverArrow,
+  PopoverClose,
+} from '@radix-ui/react-popover';
+
+Popover.displayName = 'RadixPopover';
+PopoverAnchor.displayName = 'RadixPopover.Anchor';
+PopoverArrow.displayName = 'RadixPopover.Arrow';
+PopoverClose.displayName = 'RadixPopover.Close';
+PopoverContent.displayName = 'RadixPopover.Content';
+PopoverPortal.displayName = 'RadixPopover.Portal';
+PopoverTrigger.displayName = 'RadixPopover.Trigger';
+
+type TPopover = typeof Popover & {
+  Anchor: typeof PopoverAnchor;
+  Arrow: typeof PopoverArrow;
+  Close: typeof PopoverClose;
+  Content: typeof PopoverContent;
+  Portal: typeof PopoverPortal;
+  Trigger: typeof PopoverTrigger;
+};
+
+export const RadixPopover = Popover as TPopover;
+RadixPopover.Anchor = PopoverAnchor;
+RadixPopover.Arrow = PopoverArrow;
+RadixPopover.Close = PopoverClose;
+RadixPopover.Content = PopoverContent;
+RadixPopover.Portal = PopoverPortal;
+RadixPopover.Trigger = PopoverTrigger;
