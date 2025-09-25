b0c26c74b15edb14531e729b1ef84f113c70204f
Aleksei Arro <polsck@gmail.com>
2025-09-25 18:14:06 +0400

    build(eslint): add typescript plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,34 +2,89 @@ import globals from 'globals';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import jsPlugin from '@eslint/js';
 import prettierPluginRecommended from 'eslint-plugin-prettier/recommended';
+import tsEslint from 'typescript-eslint';
+
+const sharedRules = {
+  'prefer-const': [
+    'warn',
+    {
+      destructuring: 'any',
+      ignoreReadBeforeAssign: false,
+    },
+  ],
+  'prettier/prettier': 'warn',
+};
+
+const jsTsCompatRules = {
+  'no-unused-vars': [
+    'warn',
+    {
+      args: 'all',
+      argsIgnorePattern: '^_',
+      caughtErrors: 'all',
+      destructuredArrayIgnorePattern: '^_',
+      ignoreRestSiblings: true,
+    },
+  ],
+};
+
+const toolSharedConfig = {
+  linterOptions: {
+    reportUnusedInlineConfigs: 'warn',
+  },
+  plugins: {
+    js: jsPlugin,
+  },
+  extends: [jsPlugin.configs.recommended, prettierPluginRecommended],
+  rules: {
+    ...sharedRules,
+    'no-unused-vars': jsTsCompatRules['no-unused-vars'],
+  },
+};
 
 export default defineConfig([
   globalIgnores(['*.html', '/dist/', '/extra/']),
   {
-    name: 'tool-configs',
-    files: ['./*.config.{c,m}js'],
+    name: 'tool-configs/esm',
+    files: ['./*.config.mjs'],
+    ...toolSharedConfig,
     languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.nodeBuiltin,
+      },
+    },
+  },
+  {
+    name: 'tool-configs/commonjs',
+    files: ['./*.config.cjs'],
+    ...toolSharedConfig,
+    languageOptions: {
+      sourceType: 'commonjs',
       globals: {
         ...globals.node,
       },
     },
-    linterOptions: {
-      reportUnusedInlineConfigs: 'warn',
+  },
+  {
+    name: 'src-config',
+    files: ['./src/**/*.ts?(x)'],
+    extends: [
+      jsPlugin.configs.recommended,
+      tsEslint.configs.recommendedTypeChecked,
+      prettierPluginRecommended,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    plugins: {
-      js: jsPlugin,
-    },
-    extends: [jsPlugin.configs.recommended, prettierPluginRecommended],
     rules: {
-      'no-unused-vars': ['warn'],
-      'prefer-const': [
-        'warn',
-        {
-          destructuring: 'any',
-          ignoreReadBeforeAssign: false,
-        },
-      ],
-      'prettier/prettier': 'warn',
+      ...sharedRules,
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': jsTsCompatRules['no-unused-vars'],
     },
   },
 ]);
