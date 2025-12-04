b7833ef88c528d5587195e83ea89931ab9803e84
Aleksei Arro <polsck@gmail.com>
2025-12-04 19:50:20 +0400

    build(deps): bump babel, core-js

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,7 +2,8 @@
 
 module.exports = {
   plugins: [
-    require.resolve('@prettier/plugin-oxc'),
+    // @TODO enable when @ianvs/prettier-plugin-sort-imports supports
+    // require.resolve('@prettier/plugin-oxc'),
     require.resolve('@awmottaz/prettier-plugin-void-html'),
     require.resolve('@ianvs/prettier-plugin-sort-imports'),
   ],
